f3b23aa4117cbdf932f482a3ee7fdf649b926b9c
JACKNTP <JACKNTP@JACKNTP-PC.mshome.net>
2012-09-19 16:38:34 +0700

    Start project

---
+++ TestProject/src/TestProjectMidlet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author JACKNTP
+ */
+public class TestProjectMidlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
