3dc1d526a1ebe5a770b3930da993cc31494ee321
Atchariya Darote <atchariya@megadict.com>
2012-09-19 16:49:52 +0700

    Start project

---
+++ TestProject/src/MyForm.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.microedition.lcdui.*;
+/**
+ *
+ * @author Boy
+ */
+public class MyForm extends Form {
+    public MyForm(){
+        super("Hello World");
+    }
+}
